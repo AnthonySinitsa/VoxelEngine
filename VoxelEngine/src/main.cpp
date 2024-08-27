34e681bd67eb7e93105590668ef49d9d35702f14
Anton <itonyemail@gmail.com>
2024-08-27 16:17:03 -0700

    proper setup

--- VoxelEngine/src/main.cpp
+++ VoxelEngine/src/main.cpp
@@ -1,22 +1,35 @@
-#include "window.h"
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
 
 #include <iostream>
 
 int main() {
-    try {
-        Window window(800, 600, "AAAAAAAAAAAA");
+    glfwInit();
 
-        while (!window.shouldClose()) {
-            // Render your Voxel Engine here
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
 
-            window.swapBuffers();
-            window.pollEvents();
-        }
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported\n";
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
     }
-    catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return 1;
-    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
 
     return 0;
 }
