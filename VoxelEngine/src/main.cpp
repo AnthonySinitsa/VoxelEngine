bfedc7c76e6942cfd13e754839cea3495e6530fc
AnthonySinitsa <itonyemail@gmail.com>
2024-08-28 01:32:57 -0700

    instancing

--- VoxelEngine/src/main.cpp
+++ VoxelEngine/src/main.cpp
@@ -1,9 +1,13 @@
+#include <vulkan/vulkan_core.h>
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
 
 class HelloTriangleApplication{
     public:
@@ -15,11 +19,8 @@ class HelloTriangleApplication{
     }
 
     private:
-
+    VkInstance instance;
     GLFWwindow* window;
-
-    const uint32_t WIDTH = 800;
-    const uint32_t HEIGHT = 600;
 
     void initWindow(){
         glfwInit();
@@ -31,7 +32,7 @@ class HelloTriangleApplication{
     }
 
     void initVulkan(){
-
+        createInstance();
     }
 
     void mainLoop(){
@@ -41,11 +42,43 @@ class HelloTriangleApplication{
     }
 
     void cleanup(){
+        vkDestroyInstance(instance, nullptr);
+
         glfwDestroyWindow(window);
 
         glfwTerminate();
     }
+
+    void createInstance(){
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello there Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+        createInfo.enabledLayerCount = 0;
+
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+        if(vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS){
+            throw std::runtime_error("failed to create instance!");
+        }
+    }
 };
+
 
 int main(){
     HelloTriangleApplication app;
