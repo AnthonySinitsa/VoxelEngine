705f9062b585de984b597a11d4a0202b3464450d
AnthonySinitsa <itonyemail@gmail.com>
2024-09-02 15:26:38 -0700

    added vulkan app class

---
+++ VoxelEngine/src/VulkanApplication.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Window.h"
+
+namespace vge {
+    class VulkanApplication {
+
+        public:
+            static constexpr int WIDTH = 800;
+            static constexpr int HEIGHT = 600;
+
+            void run(){};
+
+        private:
+            Window Window{WIDTH, HEIGHT, "screw AI"};
+    };
+}
