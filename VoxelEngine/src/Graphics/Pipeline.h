65673861e181e33e786c27c3bb2fd6b0261b1ed2
AnthonySinitsa <itonyemail@gmail.com>
2024-09-06 21:10:40 -0700

    viewport and scissor, rasterization, backface culling, MSAA, color blending

--- VoxelEngine/src/Graphics/Pipeline.h
+++ VoxelEngine/src/Graphics/Pipeline.h
@@ -6,11 +6,23 @@
 #include <cstdint>
 #include <string>
 #include <vector>
+#include <vulkan/vulkan_core.h>
 
 namespace vge{
 
-    struct PipelineConfigInfo{
-
+    struct PipelineConfigInfo {
+      VkViewport viewport;
+      VkRect2D scissor;
+      VkPipelineViewportStateCreateInfo viewportInfo;
+      VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+      VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+      VkPipelineMultisampleStateCreateInfo multisampleInfo;
+      VkPipelineColorBlendAttachmentState colorBlendAttachment;
+      VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+      VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+      VkPipelineLayout pipelineLayout = nullptr;
+      VkRenderPass renderPass = nullptr;
+      uint32_t subpass = 0;
     };
 
     class Pipeline{
