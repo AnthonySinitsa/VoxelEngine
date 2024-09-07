85f18812c457ee38158d5fbfe44770eca531e96f
AnthonySinitsa <itonyemail@gmail.com>
2024-09-06 23:41:06 -0700

    creating more of the graphics pipeline

--- VoxelEngine/src/VulkanApplication.cpp
+++ VoxelEngine/src/VulkanApplication.cpp
@@ -1,9 +1,53 @@
 #include "VulkanApplication.h"
+#include <vulkan/vulkan_core.h>
+
+
+// std
+#include <stdexcept>
 
 namespace vge{
+
+    VulkanApplication::VulkanApplication(){
+        createPipelineLayout();
+        createPipeline();
+        createCommandBuffers();
+    }
+
+    VulkanApplication::~VulkanApplication(){
+        vkDestroyPipelineLayout(vgeDevice.device(), pipelineLayout, nullptr);
+    }
+
     void VulkanApplication::run(){
         while(!vgeWindow.shouldClose()){
             glfwPollEvents();
         }
     }
+
+    void VulkanApplication::createPipelineLayout(){
+        VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
+        pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
+        pipelineLayoutInfo.setLayoutCount = 0;
+        pipelineLayoutInfo.pSetLayouts = nullptr;
+        pipelineLayoutInfo.pushConstantRangeCount = 0;
+        pipelineLayoutInfo.pPushConstantRanges = nullptr;
+        if(vkCreatePipelineLayout(vgeDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayoutInfo) != VK_SUCCESS){
+            throw std::runtime_error("failed to create pipeline layout!!!");
+        }
+    }
+
+    void VulkanApplication::createPipeline(){
+        auto pipelineConfig =
+            Pipeline::defaultPipelineConfigInfo(vgeSwapChain.width(), vgeSwapChain.height());
+        pipelineConfig.renderPass = vgeSwapChain.getRenderPass();
+        pipelineConfig.pipelineLayout = pipelineLayout;
+        vgePipeline = std::make_unique<Pipeline>(
+            vgeDevice,
+            "shaders/vertex_shader.vert.spv",
+            "shaders/fragment_shader.frag.spv",
+            pipelineConfig
+        );
+    }
+
+    void VulkanApplication::createCommandBuffers(){}
+    void VulkanApplication::drawFrame(){}
 } // namespace
--- VoxelEngine/src/VulkanApplication.h
+++ VoxelEngine/src/VulkanApplication.h
@@ -3,6 +3,11 @@
 #include "Window.h"
 #include "Graphics/Pipeline.h"
 #include "Device/Device.h"
+#include "Presentation/SwapChain.h"
+
+// std
+#include <memory>
+#include <vector>
 
 namespace vge {
     class VulkanApplication {
@@ -11,16 +16,25 @@ namespace vge {
             static constexpr int WIDTH = 800;
             static constexpr int HEIGHT = 600;
 
+            VulkanApplication();
+            ~VulkanApplication();
+
+            VulkanApplication(const VulkanApplication &) = delete;
+            VulkanApplication &operator=(const VulkanApplication &) = delete;
+
             void run();
 
         private:
+            void createPipelineLayout();
+            void createPipeline();
+            void createCommandBuffers();
+            void drawFrame();
+
             Window vgeWindow{WIDTH, HEIGHT, "AAAAAAAAAA"};
             VgeDevice vgeDevice{vgeWindow};
-            Pipeline vgePipeline{
-                vgeDevice,
-                "shaders/vertex_shader.vert.spv",
-                "shaders/fragment_shader.frag.spv",
-                Pipeline::defaultPipelineConfigInfo(WIDTH, HEIGHT)
-            };
+            VgeSwapChain vgeSwapChain{vgeDevice, vgeWindow.getExtent()};
+            std::unique_ptr<Pipeline> vgePipeline;
+            VkPipelineLayout pipelineLayout;
+            std::vector<VkCommandBuffer> commandBuffers;
     };
 } // namespace
