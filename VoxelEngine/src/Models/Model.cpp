affabe2c8f38fb2627cf01afcf6aee511c79671a
AnthonySinitsa <itonyemail@gmail.com>
2024-09-09 20:22:24 -0700

    host and device memory

--- VoxelEngine/src/Models/Model.cpp
+++ VoxelEngine/src/Models/Model.cpp
@@ -1,9 +1,11 @@
 #include "Model.h"
+#include <cstddef>
 #include <vulkan/vulkan_core.h>
 
 // std
 #include <cassert>
 #include <cstdint>
+#include <cstring>
 
 namespace vge{
     Model::Model(VgeDevice& device, const std::vector<Vertex> &vertices) : vgeDevice{device}{
@@ -20,5 +22,17 @@ namespace vge{
         vertexCount = static_cast<uint32_t>(vertices.size());
         assert(vertexCount >= 3 && "Vertex count must be at least 3");
         VkDeviceSize bufferSize = sizeof(vertices[0]) * vertexCount;
+        vgeDevice.createBuffer(
+            bufferSize,
+            VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+            VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+            vertexBuffer,
+            vertexBufferMemory
+        );
+        void *data;
+        vkMapMemory(vgeDevice.device(), vertexBufferMemory, 0, bufferSize, 0, &data);
+        memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
+        vkUnmapMemory(vgeDevice.device(), vertexBufferMemory);
+
     }
 }
