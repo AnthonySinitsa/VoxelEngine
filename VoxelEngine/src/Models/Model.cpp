7e1b023d92ed4d08039c642d69a199b21d922b5f
AnthonySinitsa <itonyemail@gmail.com>
2024-09-09 20:29:56 -0700

    implemented bind, draw, binding and attribute descriptions

--- VoxelEngine/src/Models/Model.cpp
+++ VoxelEngine/src/Models/Model.cpp
@@ -33,6 +33,34 @@ namespace vge{
         vkMapMemory(vgeDevice.device(), vertexBufferMemory, 0, bufferSize, 0, &data);
         memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
         vkUnmapMemory(vgeDevice.device(), vertexBufferMemory);
+    }
 
+
+    void Model::bind(VkCommandBuffer commandBuffer){
+        VkBuffer buffers[] = {vertexBuffer};
+        VkDeviceSize offsets[] = {0};
+        vkCmdBindVertexBuffers(commandBuffer, 0, 1, buffers, offsets);
+    }
+
+    void Model::draw(VkCommandBuffer commandBuffer){
+        vkCmdDraw(commandBuffer, vertexCount, 1, 0, 0);
+    }
+
+
+    std::vector<VkVertexInputBindingDescription> Model::Vertex::getBindingDescriptions(){
+        std::vector<VkVertexInputBindingDescription> bindingDescriptions(1);
+        bindingDescriptions[0].binding = 0;
+        bindingDescriptions[0].stride = sizeof(Vertex);
+        bindingDescriptions[0].inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
+        return bindingDescriptions;
+    }
+
+    std::vector<VkVertexInputAttributeDescription> Model::Vertex::getAttributeDescriptions(){
+        std::vector<VkVertexInputAttributeDescription> attributeDescriptions(1);
+        attributeDescriptions[0].binding = 0;
+        attributeDescriptions[0].location = 0;
+        attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attributeDescriptions[0].offset = 0;
+        return attributeDescriptions;
     }
 }
