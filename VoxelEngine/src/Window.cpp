4663347885a78b8c100564cd49ef4f2e57ada989
AnthonySinitsa <itonyemail@gmail.com>
2024-09-10 18:00:41 -0700

    window resize calllback

--- VoxelEngine/src/Window.cpp
+++ VoxelEngine/src/Window.cpp
@@ -15,15 +15,25 @@ namespace vge {
     void Window::initWindow(){
         glfwInit();
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
 
         window = glfwCreateWindow(width, height, windowName.c_str(), nullptr, nullptr);
+        glfwSetWindowUserPointer(window, this);
+        glfwSetFramebufferSizeCallback(window, frameBufferResizeCallback);
     }
 
     void Window::createWindowSurface(VkInstance instance, VkSurfaceKHR *surface){
         if(glfwCreateWindowSurface(instance, window, nullptr, surface) != VK_SUCCESS) {
             throw std::runtime_error("failed to create window surface");
         }
+    }
+
+
+    void Window::frameBufferResizeCallback(GLFWwindow *window, int width, int height){
+        auto vgeWindow = reinterpret_cast<Window *>(glfwGetWindowUserPointer(window));
+        vgeWindow->frameBufferResized = true;
+        vgeWindow->width = width;
+        vgeWindow->height = height;
     }
 
 } // namespace
