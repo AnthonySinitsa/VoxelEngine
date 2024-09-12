632a12495939153824343bc4d5007014d8245c8c
AnthonySinitsa <itonyemail@gmail.com>
2024-09-12 01:08:53 -0700

    writing push constant data

--- VoxelEngine/src/VulkanApplication.cpp
+++ VoxelEngine/src/VulkanApplication.cpp
@@ -5,12 +5,21 @@
 #include <memory>
 #include <vulkan/vulkan_core.h>
 
+// libs
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
 
 // std
 #include <stdexcept>
 #include <array>
 
 namespace vge{
+
+    struct SimplePushConstantData{
+        glm::vec2 offset;
+        glm::vec3 color;
+    };
 
     VulkanApplication::VulkanApplication(){
         loadModels();
@@ -44,12 +53,17 @@ namespace vge{
 
 
     void VulkanApplication::createPipelineLayout(){
+        VkPushConstantRange pushConstantRange{};
+        pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
+        pushConstantRange.offset = 0;
+        pushConstantRange.size = sizeof(SimplePushConstantData);
+
         VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
         pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
         pipelineLayoutInfo.setLayoutCount = 0;
         pipelineLayoutInfo.pSetLayouts = nullptr;
-        pipelineLayoutInfo.pushConstantRangeCount = 0;
-        pipelineLayoutInfo.pPushConstantRanges = nullptr;
+        pipelineLayoutInfo.pushConstantRangeCount = 1;
+        pipelineLayoutInfo.pPushConstantRanges = &pushConstantRange;
         if (vkCreatePipelineLayout(vgeDevice.device(), &pipelineLayoutInfo, nullptr, &pipelineLayout) != VK_SUCCESS){
             throw std::runtime_error("failed to create pipeline layout!!!");
         }
@@ -158,7 +172,22 @@ namespace vge{
 
         vgePipeline->bind(commandBuffers[imageIndex]);
         vgeModel->bind(commandBuffers[imageIndex]);
-        vgeModel->draw(commandBuffers[imageIndex]);
+
+        for(int j = 0; j < 4; ++j){
+            SimplePushConstantData push{};
+            push.offset = {0.0f, -0.4f + j * 0.25f};
+            push.color = {0.0f, 0.0f, 0.2f + 0.2f * j};
+
+            vkCmdPushConstants(
+                commandBuffers[imageIndex],
+                pipelineLayout,
+                VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT,
+                0,
+                sizeof(SimplePushConstantData),
+                &push
+            );
+            vgeModel->draw(commandBuffers[imageIndex]);
+        }
 
         vkCmdEndRenderPass(commandBuffers[imageIndex]);
         if(vkEndCommandBuffer(commandBuffers[imageIndex]) != VK_SUCCESS){
