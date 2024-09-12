ebf4d391d80dc3dbd0752fb35f946e37565ad150
AnthonySinitsa <itonyemail@gmail.com>
2024-09-12 01:18:46 -0700

    messing round with motion

--- VoxelEngine/src/VulkanApplication.cpp
+++ VoxelEngine/src/VulkanApplication.cpp
@@ -136,6 +136,9 @@ namespace vge{
 
 
     void VulkanApplication::recordCommandBuffer(int imageIndex){
+        static int frame = 0;
+        frame = (frame + 1) % 1000;
+
         VkCommandBufferBeginInfo beginInfo{};
         beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
 
@@ -152,7 +155,7 @@ namespace vge{
         renderPassInfo.renderArea.extent = vgeSwapChain->getSwapChainExtent();
 
         std::array<VkClearValue, 2> clearValues{};
-        clearValues[0].color = {0.1f, 0.1f, 0.1f, 1.0f};
+        clearValues[0].color = {0.01f, 0.01f, 0.01f, 1.0f};
         clearValues[1].depthStencil = {1.0f, 0};
         renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
         renderPassInfo.pClearValues = clearValues.data();
@@ -175,7 +178,7 @@ namespace vge{
 
         for(int j = 0; j < 4; ++j){
             SimplePushConstantData push{};
-            push.offset = {0.0f, -0.4f + j * 0.25f};
+            push.offset = {0-.5f + frame * 0.002f, -0.4f + j * 0.25f};
             push.color = {0.0f, 0.0f, 0.2f + 0.2f * j};
 
             vkCmdPushConstants(
