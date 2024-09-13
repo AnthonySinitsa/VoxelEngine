dc4477db2cafb1420e114bb7f5f31447ce73bd35
AnthonySinitsa <itonyemail@gmail.com>
2024-09-13 00:33:41 -0700

    rotating the triangle animation

--- VoxelEngine/src/VulkanApplication.cpp
+++ VoxelEngine/src/VulkanApplication.cpp
@@ -194,7 +194,9 @@ namespace vge{
     void VulkanApplication::renderGameObjects(VkCommandBuffer commandBuffer) {
         vgePipeline->bind(commandBuffer);
 
-        for(auto& obj: gameObjects){
+        for(auto& obj : gameObjects){
+            obj.transform2d.rotation = glm::mod(obj.transform2d.rotation + 0.01f, glm::two_pi<float>());
+
             SimplePushConstantData push{};
             push.offset = obj.transform2d.translation;
             push.color = obj.color;
