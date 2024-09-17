65b29ee0843d2a9a4682b5d0681ba4de29427f87
AnthonySinitsa <itonyemail@gmail.com>
2024-09-16 17:02:37 -0700

    added a rotating cube

--- VoxelEngine/src/VulkanApplication.cpp
+++ VoxelEngine/src/VulkanApplication.cpp
@@ -36,21 +36,73 @@ namespace vge{
     }
 
 
+    // temporary helper function, creates a 1x1x1 cube centered at offset
+    std::unique_ptr<Model> createCubeModel(VgeDevice& device, glm::vec3 offset) {
+      std::vector<Model::Vertex> vertices{
+
+          // left face (white)
+          {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+          {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+          {{-.5f, -.5f, .5f}, {.9f, .9f, .9f}},
+          {{-.5f, -.5f, -.5f}, {.9f, .9f, .9f}},
+          {{-.5f, .5f, -.5f}, {.9f, .9f, .9f}},
+          {{-.5f, .5f, .5f}, {.9f, .9f, .9f}},
+
+          // right face (yellow)
+          {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+          {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
+          {{.5f, -.5f, .5f}, {.8f, .8f, .1f}},
+          {{.5f, -.5f, -.5f}, {.8f, .8f, .1f}},
+          {{.5f, .5f, -.5f}, {.8f, .8f, .1f}},
+          {{.5f, .5f, .5f}, {.8f, .8f, .1f}},
+
+          // top face (orange, remember y axis points down)
+          {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+          {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+          {{-.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+          {{-.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+          {{.5f, -.5f, -.5f}, {.9f, .6f, .1f}},
+          {{.5f, -.5f, .5f}, {.9f, .6f, .1f}},
+
+          // bottom face (red)
+          {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+          {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+          {{-.5f, .5f, .5f}, {.8f, .1f, .1f}},
+          {{-.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+          {{.5f, .5f, -.5f}, {.8f, .1f, .1f}},
+          {{.5f, .5f, .5f}, {.8f, .1f, .1f}},
+
+          // nose face (blue)
+          {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+          {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+          {{-.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+          {{-.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+          {{.5f, -.5f, 0.5f}, {.1f, .1f, .8f}},
+          {{.5f, .5f, 0.5f}, {.1f, .1f, .8f}},
+
+          // tail face (green)
+          {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+          {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+          {{-.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+          {{-.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+          {{.5f, -.5f, -0.5f}, {.1f, .8f, .1f}},
+          {{.5f, .5f, -0.5f}, {.1f, .8f, .1f}},
+
+      };
+      for (auto& v : vertices) {
+        v.position += offset;
+      }
+      return std::make_unique<Model>(device, vertices);
+    }
+
+
     void VulkanApplication::loadGameObjects(){
-        std::vector<Model::Vertex> vertices{
-            {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
-            {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
-            {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}},
-        };
-        auto vgeModel = std::make_shared<Model>(vgeDevice, vertices);
+        std::shared_ptr<Model> vgeModel = createCubeModel(vgeDevice, {.0f, .0f, .0f});
 
-        auto triangle = GameObject::createGameObject();
-        triangle.model = vgeModel;
-        triangle.color = {.1f, .8f, .1f};
-        triangle.transform2d.translation.x = .2f;
-        triangle.transform2d.scale = {2.f, .5f};
-        triangle.transform2d.rotation = .25f * glm::two_pi<float>();
-
-        gameObjects.push_back(std::move(triangle));
+        auto cube = GameObject::createGameObject();
+        cube.model = vgeModel;
+        cube.transform.translation = {.0f, .0f, .5f};
+        cube.transform.scale = {.5, .5f, .5f};
+        gameObjects.push_back(std::move(cube));
     }
 } // namespace
