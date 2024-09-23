77ed7f85e4e360eec5f1644ffcada58a4af9dfad
AnthonySinitsa <itonyemail@gmail.com>
2024-09-22 23:58:38 -0700

    staging buffer for createIndexBuffers()

--- VoxelEngine/src/Models/Model.cpp
+++ VoxelEngine/src/Models/Model.cpp
@@ -28,17 +28,34 @@ namespace vge{
         vertexCount = static_cast<uint32_t>(vertices.size());
         assert(vertexCount >= 3 && "Vertex count must be at least 3");
         VkDeviceSize bufferSize = sizeof(vertices[0]) * vertexCount;
+
+        VkBuffer stagingBuffer;
+        VkDeviceMemory stagingBufferMemory;
         vgeDevice.createBuffer(
             bufferSize,
-            VK_BUFFER_USAGE_VERTEX_BUFFER_BIT,
+            VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
             VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+            stagingBuffer,
+            stagingBufferMemory
+        );
+
+        void *data;
+        vkMapMemory(vgeDevice.device(), stagingBufferMemory, 0, bufferSize, 0, &data);
+        memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
+        vkUnmapMemory(vgeDevice.device(), stagingBufferMemory);
+
+        vgeDevice.createBuffer(
+            bufferSize,
+            VK_BUFFER_USAGE_VERTEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+            VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
             vertexBuffer,
             vertexBufferMemory
         );
-        void *data;
-        vkMapMemory(vgeDevice.device(), vertexBufferMemory, 0, bufferSize, 0, &data);
-        memcpy(data, vertices.data(), static_cast<size_t>(bufferSize));
-        vkUnmapMemory(vgeDevice.device(), vertexBufferMemory);
+
+        vgeDevice.copyBuffer(stagingBuffer, vertexBuffer, bufferSize);
+
+        vkDestroyBuffer(vgeDevice.device(), stagingBuffer, nullptr);
+        vkFreeMemory(vgeDevice.device(), stagingBufferMemory, nullptr);
     }
 
 
@@ -51,17 +68,34 @@ namespace vge{
         }
 
         VkDeviceSize bufferSize = sizeof(indices[0]) * indexCount;
+
+        VkBuffer stagingBuffer;
+        VkDeviceMemory stagingBufferMemory;
         vgeDevice.createBuffer(
             bufferSize,
-            VK_BUFFER_USAGE_INDEX_BUFFER_BIT,
+            VK_BUFFER_USAGE_TRANSFER_SRC_BIT,
             VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+            stagingBuffer,
+            stagingBufferMemory
+        );
+
+        void *data;
+        vkMapMemory(vgeDevice.device(), stagingBufferMemory, 0, bufferSize, 0, &data);
+        memcpy(data, indices.data(), static_cast<size_t>(bufferSize));
+        vkUnmapMemory(vgeDevice.device(), stagingBufferMemory);
+
+        vgeDevice.createBuffer(
+            bufferSize,
+            VK_BUFFER_USAGE_INDEX_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+            VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
             indexBuffer,
             indexBufferMemory
         );
-        void *data;
-        vkMapMemory(vgeDevice.device(), indexBufferMemory, 0, bufferSize, 0, &data);
-        memcpy(data, indices.data(), static_cast<size_t>(bufferSize));
-        vkUnmapMemory(vgeDevice.device(), indexBufferMemory);
+
+        vgeDevice.copyBuffer(stagingBuffer, indexBuffer, bufferSize);
+
+        vkDestroyBuffer(vgeDevice.device(), stagingBuffer, nullptr);
+        vkFreeMemory(vgeDevice.device(), stagingBufferMemory, nullptr);
     }
 
 
